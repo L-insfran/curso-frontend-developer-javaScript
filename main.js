b7dcbef966e98d7e54e2e4dea6565820e6c1f82c
Leandro Insfran <leandro.insfran@systelec.com>
2023-03-12 23:04:46 -0300

    Carrito de comptas

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const BurguerMenu = document.querySelector(".menu");
+const MenuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 BurguerMenu.addEventListener("click", toggleMobileMenu);
+MenuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu(){
+  const isAsideClose = aside.classList.contains("inactive");
+  if (!isAsideClose) {
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+  const isAsideClose = aside.classList.contains("inactive");
+  if (!isAsideClose) {
+    aside.classList.add("inactive");
+  }
+  
   mobileMenu.classList.toggle("inactive");
+
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuOpen = !mobileMenu.classList.contains("inactive")
+  
+  if (isMobileMenuOpen){
+    mobileMenu.classList.add("inactive")
+  }
+  
+  aside.classList.toggle("inactive");
 }
